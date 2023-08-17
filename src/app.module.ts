6aacc0d9c281635c7cd4339e574fc9a00bdd994d
Charles <babouwitonze921@gmail.com>
2023-08-17 14:57:58 +0200

    Removing environment variables 😅

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ dotenv.config();
 @Module({
   imports: [TypeOrmModule.forRoot({
     type:'postgres',
-    host:'localhost',
+    host:process.env.DB_HOST,
     port: 5432,
     password: process.env.DB_PASSWORD,
     username: process.env.DB_USER,
